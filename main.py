28b76c7b40977c79a294080007d5e8d8198f4adf
conor <conorlynam1234@gmail.com>
2024-08-03 11:48:14 +0100

    pandas

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-
 file_path = 'Kilkenny tankering. 2023 (5).xlsx'
-
 excel_data = pd.ExcelFile(file_path)
 
 sheet_name = excel_data.sheet_names[8]
-print(sheet_name)
+
+dataframe = pd.read_excel(file_path, sheet_name=sheet_name)
+
