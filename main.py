b9d378b0cc26772f240d4ddaf547b4a234d5d369
conor <conorlynam1234@gmail.com>
2024-08-03 11:12:50 +0100

    venv

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("i kiss boys")
+import pandas as pd
+
+
+file_path = 'Kilkenny tankering. 2023 (5).xlsx'
+
+excel_data = pd.ExcelFile(file_path)
+
+sheet_name = excel_data.sheet_names[8]
+print(sheet_name)
